c46bbf333dd24c77e5e3b2184f2aa313560f062a
Williams Malachy <billmal071@gmail.com>
2023-10-16 13:58:14 +0100

    chore: add types

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: "standard-with-typescript",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
