abcdc90ecaa81b630388aab73f7ec630f9f4417e
Williams Malachy <billmal071@gmail.com>
2023-10-17 15:14:40 +0100

    feat: add api health check

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("combined"));
 
+app.get("/", (_, res) => {
+  res.status(200).json({
+      message: "Welcome to Codespacers backend",
+      status: "200",
+      success: true,
+      link: "/api/v1/health"
+  })
+})
+
+app.get("/api/v1/health", (_, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "API is up and running",
+    success: true
+  })
+})
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(3000, () => {
 	logger.info(chalk.bgRed.blue.bold("Server is running on port 3000"));
+});
+
+process.on("uncaughtException", (err) => {
+	logger.error(err, " : ", err.stack);
+});
+
+process.on("unhandledRejection", (err) => {
+	logger.error(err);
 });
