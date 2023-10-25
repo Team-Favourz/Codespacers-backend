e3edcdac92963a993448a34a1db345a9975f68b5
Kingsley Ibe <kingsley.ibe@decagon.dev>
2023-10-25 02:11:32 +0100

    added create folder routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,14 @@
+
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from "express";
-import { registerUser, userLogIn, logout } from "../controllers/index";
+import { registerUser, userLogIn, logout, createFolder } from "../controllers/index";
 import { verifyTokenFromCookie } from "../middlewares/token_verifier";
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", verifyTokenFromCookie, userLogIn);
 router.post("/logout", verifyTokenFromCookie, logout);
+router.post("/createfolder", createFolder)
 
 export default router;
 
